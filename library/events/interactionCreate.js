f9854857b4cde03781020757b142670abe6372cb
Gökhan Can <55600314+chimpdev@users.noreply.github.com>
2022-09-10 00:03:24 +0300

    Update interactionCreate.js

--- library/events/interactionCreate.js
+++ library/events/interactionCreate.js
@@ -1,6 +1,6 @@
 const { InteractionType } = require('discord.js');
 
-module.exports = async interaction => {
-  if (interaction.type === InteractionType.ApplicationCommand && interaction.client.commands.some(cmd => cmd.data.name === interaction.commandName)) interaction.client.commands.find(cmd => cmd.data.name === interaction.commandName).execute(interaction);
-  if (interaction.type === InteractionType.ModalSubmit && interaction.client.commands.find(cmd => cmd.events?.submit[interaction.customId])) return interaction.client.commands.find(cmd => cmd.events?.submit[interaction.customId])?.events.submit[interaction.customId](interaction);
+module.exports = async (client, interaction) => {
+  if (interaction.type === InteractionType.ApplicationCommand && client.commands.some(cmd => cmd.data.name === interaction.commandName)) client.commands.find(cmd => cmd.data.name === interaction.commandName).execute(interaction);
+  if (interaction.type === InteractionType.ModalSubmit && client.commands.find(cmd => cmd.events?.submit[interaction.customId])) return client.commands.find(cmd => cmd.events?.submit[interaction.customId])?.events.submit[interaction.customId](interaction);
 };
